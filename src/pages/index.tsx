f2ecd3457e772f92ed864ceba95b701153f307dc
shpark <bingbingpa@gmail.com>
2021-09-09 12:02:15 +0900

    feat: 메인 페이지

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,32 @@
 import React, { FunctionComponent } from 'react';
-import Text from 'components/Text';
-import { Link } from 'gatsby';
+import styled from '@emotion/styled';
+import GlobalStyle from 'components/common/globalStyle';
+import Introduction from 'components/main/introduction';
+import Footer from 'components/common/footer';
+import CategoryList from 'components/main/categoryList';
+import PostList from 'components/main/postList';
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
 
 const IndexPage: FunctionComponent = function () {
   return (
-    <div>
-      <Text text="Hello, World!" />
-      <Link to="/info/">To Info</Link>
-    </div>
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <PostList />
+      <Footer />
+    </Container>
   );
 };
 
